00e9acb743b8acbafd5d0f56c479182e7f1ae017
Dudi Levy <dudi@elv8.pro>
2021-06-30 13:54:18 +0000

    add root install version of docker

--- scripts/ubuntu-docker-install.sh
+++ scripts/ubuntu-docker-install.sh
@@ -2,23 +2,31 @@
 sudo apt-get -y remove docker docker-engine docker.io containerd runc
 sudo rm -rf /var/lib/docker
 sudo rm -rf /var/lib/containerd
-sudo apt-get update && sudo apt-get -y install apt-transport-https \
+
+sudo groupadd -f docker
+sudo usermod -aG docker $USER
+newgrp docker
+sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
+sudo chmod g+rwx "$HOME/.docker" -R
+
+sudo apt-get update
+sudo apt-get -y install apt-transport-https \
                         ca-certificates \
                         curl \
                         gnupg \
-                        lsb-release \
-                        uidmap
+                        lsb-release
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -fo /usr/share/keyrings/docker-archive-keyring.gpg
+
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update && sudo apt-get install -y docker-ce-rootless-extras docker-ce docker-ce-cli containerd.io
-dockerd-rootless-setuptool.sh install
-systemctl --user enable docker
-sudo loginctl enable-linger $(whoami)
-echo 'export PATH=/usr/bin:$PATH' >> ~/.bashrc
-echo 'export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock' >> ~/.bashrc
 
+sudo apt-get update 
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
